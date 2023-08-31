054bb5cd8a7fcc62fda7c2fc695360e20192e03e
Tiny-Prince <rlaxodnjs6574@gmail.com>
2023-08-31 18:16:45 +0900

    Feature : "Assimp, XTK 추가 등"

--- Framework/Copy.bat
+++ Framework/Copy.bat
@@ -1,5 +1,9 @@
-// 명령어		옵션		원본파일 위치					사본파일 저장 위치
-	
-xcopy		/y		.\Engine\Public\*.h						.\Reference\Headers\		
-xcopy		/y		.\Engine\Bin\*.lib						.\Reference\Librarys\		
-xcopy		/y		.\Engine\Bin\*.dll						.\Client\Bin\
+
+
+//// xcopy /옵션 .복사할파일이 있는 경로			.복사받을경로
+
+xcopy /y/s .\Engine\Public\*.*				.\Reference\Headers\
+xcopy /y .\Engine\Bin\*.lib					.\Reference\Librarys\
+xcopy /y .\Engine\ThirdPartyLib\*.lib		.\Reference\Librarys\
+xcopy /y .\Engine\Bin\*.dll					.\Client\Bin\
+xcopy /y .\Engine\Bin\ShaderFiles\*.hlsl	.\Client\Bin\ShaderFiles\
--- Framework/Engine/Public/Engine_Defines.h
+++ Framework/Engine/Public/Engine_Defines.h
@@ -1,20 +1,47 @@
+
 #pragma once
 
+#pragma warning (disable : 4005)
 #pragma warning (disable : 4251)
+
 #include <d3d11.h>
+#include <d3dcompiler.h>
+#include <FX11/d3dx11effect.h>
+#include <DirectXTK/DDSTextureLoader.h>
+#include <DirectXTK/WICTextureLoader.h>
+#include <DirectXTK/ScreenGrab.h>
+
+#include <DirectXTK/PrimitiveBatch.h>
+#include <DirectXTK/Effects.h>
+#include <DirectXTK/VertexTypes.h>
+#include <DirectXTK/SpriteBatch.h>
+#include <DirectXTK/SpriteFont.h>
+
+#include <DirectXCollision.h>
+
+
+#include <Assimp/scene.h>
+#include <Assimp/Importer.hpp>
+#include <Assimp/postprocess.h>
+
+#define DIRECTINPUT_VERSION 0x0800
+#include <dinput.h>
 
 #include <DirectXMath.h>
 using namespace DirectX;
 
-#include <vector> /* 배열 */
-#include <list> /* 더블링크드리스트 */
-#include <map> /* 이진트리 */
-#include <unordered_map> /* 해시테이블 */
+#include <vector>
+#include <list>
+#include <map>
 #include <algorithm>
+#include <typeinfo>
 
-#include "Engine_Typedef.h"
+
+#include <process.h>
+
 #include "Engine_Macro.h"
 #include "Engine_Struct.h"
+#include "Engine_Typedef.h"
 #include "Engine_Function.h"
 
 #ifdef _DEBUG
@@ -32,9 +59,7 @@ using namespace DirectX;
 
 #endif // _DEBUG
 
-
+using namespace std;
 using namespace Engine;
 
 
-
-using namespace std;
